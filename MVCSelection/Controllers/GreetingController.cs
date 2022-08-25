c316d5aad0537c17ddb00b101c13318aeaf9721f
Joseph Hooker <jcolehooker@gmail.com>
2022-08-25 16:51:18 -0500

    First

---
+++ MVCSelection/Controllers/GreetingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCSelection.Controllers
+{
+    public class GreetingController : Controller
+    {
+        public string Index()
+        {
+            return "Welcome to the index page";
+        }
+
+        public string Hello()
+        {
+            return "Who is there?";
+        }
+    }
+
+}
---
+++ MVCSelection/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCSelection.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
