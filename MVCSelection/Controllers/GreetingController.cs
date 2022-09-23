ba3ba95d110f4e8524a500e40b0dd6325a7f9028
Joseph Hooker <jcolehooker@gmail.com>
2022-09-23 18:50:48 -0500

    Expanded models, controllers, and page display

--- MVCSelection/Controllers/GreetingController.cs
+++ MVCSelection/Controllers/GreetingController.cs
@@ -1,14 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCSelection.Models;
 
 namespace MVCSelection.Controllers
 {
     public class GreetingController : Controller
     {
-        public string Index()
+        public IActionResult Index()
         {
-            return "Welcome to the index page";
+            DogViewModel dog = new DogViewModel() { Name = "Zambie", Age = 8 };
+            return View(dog);
         }
 
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            return View("Index");
+        }
         public string Hello()
         {
             return "Who is there?";
